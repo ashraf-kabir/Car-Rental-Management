489a63d311c5d512ff3798c3c8097ef773e771b1
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-04-12 21:17:59 +0600

    changes

--- includes/header.php
+++ includes/header.php
@@ -99,16 +99,19 @@
                     </form>
                 </div>
             </div>
+
             <div class="collapse navbar-collapse" id="navigation">
                 <ul class="nav navbar-nav">
-                    <li><a href="index.php">Home</a></li>
+                    <li class="nav-item">
+                        <a href="index.php" class="nav-link">Home</a>
+                    </li>
                     <li><a href="page.php?type=aboutus">About Us</a></li>
                     <li><a href="car-listing.php">Car Listing</a>
                     <li><a href="page.php?type=faqs">FAQs</a></li>
                     <li><a href="contact-us.php">Contact Us</a></li>
-
                 </ul>
             </div>
+
         </div>
     </nav>
     <!-- Navigation end -->
