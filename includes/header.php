3cb6b626876b093a91911f0e95c9188ca1be945f
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-07-22 19:42:52 +0600

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -14,7 +14,7 @@
                         <div class="header_widgets">
                             <div class="circle_icon"><i class="fa fa-phone" aria-hidden="true"></i></div>
                             <p class="uppercase_text">Service Helpline Call Us:</p>
-                            <a href="tel:61-1234-5678-09">+880 1751336666</a></div>
+                            <a href="tel:61-1234-5678-09">+880 1234567890</a></div>
                         <div class="social-follow">
                             <ul>
                                 <li><a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
